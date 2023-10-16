af9acda99b01c9b9dd4df048b09200fb27e0d755
Steve H <102484505+probablyacai@users.noreply.github.com>
2023-10-16 16:12:03 -0400

    Update Example.js

--- Example.js
+++ Example.js
@@ -1,11 +1,9 @@
-import Query from 'query-ast';
-
 export default function Example({ file, element }) {
     if (file.endsWith('.jsx')) {
         return {
             condition: true,
-            defaultExport: ({ file1, element1 }) => {
-                const node = element1.declaration.body;
+            defaultExport: ({ file, element }) => {
+                const node = element.declaration.body;
                 node.body[1].argument.children[1].children[1].openingElement.children[3].attributes[1].src = "https://media.bizj.us/view/img/12093761/mdp06464*1200xx1333-1333-334-0.jpg";
             }
         };
